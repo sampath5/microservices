98110eb8101c58e6044a3e1883873b5587c3d363
sampath5 <46045003+sampath5@users.noreply.github.com>
2020-05-19 11:16:35 +0530

    Update PaymentApplication.java

--- Payment/src/main/java/com/microservice/demo/PaymentApplication.java
+++ Payment/src/main/java/com/microservice/demo/PaymentApplication.java
@@ -3,6 +3,7 @@ package com.microservice.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +19,7 @@ public class PaymentApplication {
 	}
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
